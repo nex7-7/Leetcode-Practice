6a800e62a2a02d2cdac0aafd7dd441544bd25d8c
nex7-7 <snehil.sinha784@gmail.com>
2024-07-23 00:51:51 +0530

    22/07/24

--- Scratch.cpp
+++ Scratch.cpp
@@ -1,49 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int maxSkill(int s, int n, int d, vector<vector<int>> arr)
+int mrt()
 {
-    int finalSkill = s;
-    vector<vector<int>> sort;
-    int maxSkillGain = 0;
-    int maxSkillGainIndex = 0;
-    for (int i = 0; i < d; i++)
+    int n, ti, li, sum = 0, element = 0;
+    vector<vector<int>> time;
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < arr.size(); j++)
-        {
-            if (arr[j][0] > finalSkill)
-                continue;
-            else if (arr[j][0] <= finalSkill)
-            {
-                vector<int> *element = new vector<int>;
-                element->push_back(j);
-                element->push_back(arr[j][0]);
-                element->push_back(arr[j][1]);
-                sort.push_back(*element);
-            }
-        }
-        if (!sort.empty())
-        {
-            for (int k = 0; k < sort.size(); k++)
-            {
-                if (sort[k][2] > maxSkillGain)
-                {
-                    maxSkillGain = sort[k][2];
-                    maxSkillGainIndex = sort[k][0];
-                }
-            }
-            arr.erase(arr.begin() + maxSkillGainIndex);
-            finalSkill += maxSkillGain;
-            sort.clear();
-        }
+        cin >> ti >> li;
+        time.push_back({li - ti, ti});
     }
-    return finalSkill;
+    sort(time.begin(), time.end());
+    for (int i = 0; i < n; i++)
+    {
+        element += time[i][0];
+        sum += element + time[i][1];
+    }
+    return sum / n;
 }
 
 int main()
 {
-    vector<vector<int>> arr = {{22, 8}, {13, 6}, {100, 4}, {19, 2}, {33, 5}, {37, 9}};
-    cout << maxSkill(25, 6, 3, arr) << endl;
-    return 0;
+    cout << mrt();
 }
